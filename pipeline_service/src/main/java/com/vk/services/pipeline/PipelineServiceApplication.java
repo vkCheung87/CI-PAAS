c9193c7b81cfdbfb24bb0727f444c0afa563ef68
Zhang YongJian <yjzhang@thoughtworks.com>
2018-10-08 10:58:54 +0800

    Add new service, pipeline service.

---
+++ pipeline_service/src/main/java/com/vk/services/pipeline/PipelineServiceApplication.java
@@ -0,0 +1,14 @@
+package com.vk.services.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PipelineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineServiceApplication.class, args);
+	}
+}
